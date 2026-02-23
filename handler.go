107f877684794be21a6eaafbd0c844b50db72336
Aziz Kakhorov <kaxopob.azizuz@gmail.com>
2026-02-23 22:49:33 +0500

    create handler

---
+++ handler.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type ErrorRespose struct {
+	Message string `json:"message"`
+}
+
+type Handler struct {
+	storage Storage
+}
+
+func NewHandler(storage Storage) *Handler {
+	return &Handler{storage: storage}
+}
+
+func (h *Handler) CreateEmployee(c *gin.Context) {
+	var employee Employee
+
+	if err := c.BindJSON(&employee); err != nil {
+		fmt.Printf("falled to bind employee: %s\n", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorRespose{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	h.storage.Insert(&employee)
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": employee.ID,
+	})
+}
+
+func (h *Handler) UpdateEmployee(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		fmt.Printf("faled to convert id param to int: %s\n", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorRespose{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	var employe Employee
+
+	if err := c.BindJSON(&employe); err != nil {
+		fmt.Printf("faled to bind employee: %s\n", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorRespose{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	h.storage.Update(id, employe)
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": employe.ID,
+	})
+}
+
+func (h *Handler) GetEmployee(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		fmt.Printf("faled convert id param to int: %s\n", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorRespose{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	employee, err := h.storage.Get(id)
+	if err != nil {
+		fmt.Printf("faled to get employee: %s\n", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorRespose{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, employee)
+}
+
+func (h *Handler) DeleteEmploye(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		fmt.Printf("faled convert id param to int: %s\n", err.Error())
+		c.JSON(http.StatusBadRequest, ErrorRespose{
+			Message: err.Error(),
+		})
+		return
+	}
+
+	h.storage.Delete(id)
+
+	c.String(http.StatusOK, "employee deleted")
+}
